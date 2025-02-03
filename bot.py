8d10157adc1352d81fcb55c1bd800b402ed09885
Nikita Kremnev <nkremnev3003@gmail.com>
2025-02-03 19:19:57 +0300

    refactor

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import asyncio
 from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
 from aiogram.types import WebAppInfo, KeyboardButton, ReplyKeyboardMarkup
 
 TOKEN = "7892759186:AAF1j-Gsi4hJY8LxrI9RgTWeumj1YeDQNfk"
@@ -7,16 +8,23 @@ TOKEN = "7892759186:AAF1j-Gsi4hJY8LxrI9RgTWeumj1YeDQNfk"
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
-@dp.message(commands=['start'])
+
+@dp.message(Command("start"))
 async def send_welcome(message: types.Message):
-    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-    web_app = WebAppInfo(url="https://yourusername.github.io/miniapp/")
+    web_app = WebAppInfo(url="https://amvera-nkremnev3003-run-shipping.io")
     button = KeyboardButton(text="Открыть Mini App", web_app=web_app)
-    keyboard.add(button)
+
+    keyboard = ReplyKeyboardMarkup(
+        keyboard=[[button]],
+        resize_keyboard=True
+    )
+
     await message.answer("Нажмите кнопку, чтобы открыть приложение:", reply_markup=keyboard)
+
 
 async def main():
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
